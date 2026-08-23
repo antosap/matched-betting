50db9a4028f787e7f3211b6bf3699c496d5bd357
antosap <43786489+antosap@users.noreply.github.com>
2026-08-23 20:08:47 +0200

    Update opportunityProvider.ts

--- lib/providers/opportunityProvider.ts
+++ lib/providers/opportunityProvider.ts
@@ -1,28 +1,17 @@
-import { calculateOpportunity } from "@/lib/matchedBetting/opportunityCalculator";
-import { demoProvider } from "./demoProvider";
+import { findMatchedOpportunities } from "@/lib/matchedBetting/oddsMatcher";
 import { matchedBettingConfig } from "@/lib/config/matchedBetting";
+import { demoProvider } from "./demoProvider";
+
 export async function getDemoOpportunities() {
   const quotes = await demoProvider.getQuotes();
 
-  const opportunities = [];
+  return findMatchedOpportunities(quotes, {
+    exchangeCommissionPct:
+      matchedBettingConfig.defaultExchangeCommissionPct,
 
-  for (let i = 0; i < quotes.length; i++) {
-    for (let j = 0; j < quotes.length; j++) {
-      if (i === j) continue;
+    backStake:
+      matchedBettingConfig.defaultBackStake,
 
-      const back = quotes[i];
-      const lay = quotes[j];
-
-      if (!lay.exchangeId) continue;
-      if (back.exchangeId) continue;
-
-      const opportunity = calculateOpportunity(   back,   lay,   matchedBettingConfig.defaultExchangeCommissionPct,   matchedBettingConfig.defaultBackStake );
-
-      if (opportunity) {
-        opportunities.push(opportunity);
-      }
-    }
-  }
-
-  return opportunities;
+    minimumRoiPct: 0,
+  });
 }
